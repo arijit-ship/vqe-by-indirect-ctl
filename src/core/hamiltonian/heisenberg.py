023e7eee7c1795e4c0353816a7a66a30c24c1b10
tanan <toshifumi.anan@gmail.com>
2023-06-25 12:17:19 +0900

    heisenberg hamiltonian

--- src/core/hamiltonian/heisenberg.py
+++ src/core/hamiltonian/heisenberg.py
@@ -12,11 +12,9 @@ class HeisenbergHamiltonian(HamiltonianProtocol):
         self,
         n_qubits: int,
         coef: Coefficients,
-        gamma: float = 0,
     ) -> None:
         self.n_qubits = n_qubits
         self.coef = coef
-        self.gamma = gamma
 
     @property
     def type(self) -> HamiltonianModel:
@@ -33,49 +31,36 @@ class HeisenbergHamiltonian(HamiltonianProtocol):
     def create_hamiltonian(self) -> np.ndarray:
         XX = np.array(np.zeros(2**self.n_qubits))
         YY = np.array(np.zeros(2**self.n_qubits))
-        Zn = np.array(np.zeros(2**self.n_qubits))
-        for j in range(self.n_qubits - 1):
-            for k in range(self.n_qubits):
-                if j == k:
-                    if k == 0:
+        ZZ = np.array(np.zeros(2**self.n_qubits))
+        for k in range(self.n_qubits - 1):
+            for l in range(self.n_qubits):
+                if k == l:
+                    if l == 0:
                         hamiX = PauliGate.X_gate.value
                         hamiY = PauliGate.Y_gate.value
+                        hamiZ = PauliGate.Z_gate.value
                     else:
                         hamiX = np.kron(hamiX, PauliGate.X_gate.value)
                         hamiY = np.kron(hamiY, PauliGate.Y_gate.value)
+                        hamiZ = np.kron(hamiZ, PauliGate.Z_gate.value)
 
-                elif j + 1 == k:
+                elif k + 1 == l:
                     hamiX = np.kron(hamiX, PauliGate.X_gate.value)
                     hamiY = np.kron(hamiY, PauliGate.Y_gate.value)
+                    hamiZ = np.kron(hamiZ, PauliGate.Z_gate.value)
                 else:
-                    if k == 0:
+                    if l == 0:
                         hamiX = PauliGate.I_gate.value
                         hamiY = PauliGate.I_gate.value
+                        hamiZ = PauliGate.I_gate.value
                     else:
                         hamiX = np.kron(hamiX, PauliGate.I_gate.value)
                         hamiY = np.kron(hamiY, PauliGate.I_gate.value)
-            
-            if not isinstance(self.coef, tuple):
-                raise ValueError("coefficient must be tuple.")
-            XX = XX + self.coef[0][j] * (1 + self.gamma) * hamiX
-            YY = YY + self.coef[0][j] * (1 - self.gamma) * hamiY
-
-        for m in range(self.n_qubits):
-            for n in range(self.n_qubits):
-                if m == n:
-                    if n == 0:
-                        hamiZ = PauliGate.Z_gate.value
-                    else:
-                        hamiZ = np.kron(hamiZ, PauliGate.Z_gate.value)
-
-                else:
-                    if n == 0:
-                        hamiZ = PauliGate.I_gate.value
-                    else:
                         hamiZ = np.kron(hamiZ, PauliGate.I_gate.value)
+            if not isinstance(self.coef, list):
+                raise ValueError("coefficient must be list[float].")
+            XX = XX + cn[k] * hamiX
+            YY = YY + cn[k] * hamiY
+            ZZ = ZZ + cn[k] * hamiZ
 
-            if not isinstance(self.coef, tuple):
-                raise ValueError("coefficient must be tuple.")
-            Zn = Zn + self.coef[1][m] * hamiZ
-
-        return XX + YY + Zn
+        return XX + YY + ZZ
